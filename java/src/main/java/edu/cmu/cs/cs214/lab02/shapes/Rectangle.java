01aba10aa4a4c333e16adcfb5bee52e1dabcd684
Hussssar <jiaheng2@andrew.cmu.edu>
2023-09-06 03:11:04 -0400

    Encapsulation for Java

--- java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Rectangle.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/shapes/Rectangle.java
@@ -1,8 +1,8 @@
 package edu.cmu.cs.cs214.lab02.shapes;
 
 public class Rectangle implements Shape {
-    public double height;
-    public double width;
+    private double height; // change to private
+    private double width; // change to private
     
     public Rectangle(double height, double width){
         this.height = height;
